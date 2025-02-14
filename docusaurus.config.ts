945d2006a2c42fac941dbef63f761f7fc546a3b2
Bobby Galli <bobbyg603@gmail.com>
2025-02-14 01:49:27 -0500

    chore: github pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://docs.heyquotey.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/hey-quotey-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
